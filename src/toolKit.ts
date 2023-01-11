3edbea11ed4726323ad434f124490b863d44b04e
wengege1 <wengege1@jd.com>
2023-01-11 16:01:46 +0800

    添加一个func

--- src/toolKit.ts
+++ src/toolKit.ts
@@ -2,4 +2,7 @@ export class Toolkit {
   printSomething(name: string) {
     console.log("Hello " + name + ". Welcome to this toolkit.");
   }
+  alertSomething(msg: string) {
+    alert(msg)
+  }
 }
